ca0a1faae467345ec2ac0c00268ef2a966915b61
sobhan12345 <shobhan.babu1993@gmail.com>
2020-12-07 13:08:35 +0530

    maven project

---
+++ src/main/java/shobhanbabu/App.java
@@ -0,0 +1,13 @@
+package shobhanbabu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! and how are you " );
+    }
+}

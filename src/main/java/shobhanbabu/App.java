4aeb6b759f3c7ce1186596361d2639605304b4a4
sobhan12345 <53886548+sobhan12345@users.noreply.github.com>
2020-12-07 17:59:43 +0530

    Update App.java

--- src/main/java/shobhanbabu/App.java
+++ src/main/java/shobhanbabu/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! and how are you ia good where are you " );
-        System.out.println( "hello babu how are you iam good ");
+        System.out.println( "hello babu how are you iam good ")
     }
 }
